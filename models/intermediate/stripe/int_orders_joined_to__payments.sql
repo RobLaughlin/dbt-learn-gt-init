fc49a6df8d8dc6760edd787d2dc29775335d34a9
Rob Laughlin <rjlwebdev@gmail.com>
2026-06-07 12:57:40 -0700

    Modify query to use only successful payments and completed orders

--- models/intermediate/stripe/int_orders_joined_to__payments.sql
+++ models/intermediate/stripe/int_orders_joined_to__payments.sql
@@ -1,9 +1,9 @@
 WITH orders AS (
-    SELECT order_id, customer_id FROM {{ ref("stg_jaffle_shop__orders") }}
+    SELECT order_id, customer_id, status FROM {{ ref("stg_jaffle_shop__orders") }}
 ),
 
 payment AS (
-    SELECT order_id, amount_usd FROM {{ ref("stg_stripe__payment") }}
+    SELECT order_id, amount_usd, status FROM {{ ref("stg_stripe__payment") }}
 ),
 
 final AS (
@@ -13,6 +13,8 @@ final AS (
         payment.amount_usd,
     FROM orders
     LEFT JOIN payment ON orders.order_id = payment.order_id
+    WHERE payment.status = 'success'
+    AND orders.status = 'completed'
 )
 
 SELECT * FROM final
