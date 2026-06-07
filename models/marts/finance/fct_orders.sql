1fed7014bc8f5b0f72708d1d9d812b82fd5a8a0f
Rob Laughlin <rjlwebdev@gmail.com>
2026-06-07 16:34:49 -0700

    Rename intermediate models

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,1 +1,1 @@
-SELECT order_id, customer_id, amount_usd FROM {{ ref('int_orders_joined_to__payments') }}
+SELECT order_id, customer_id, amount_usd FROM {{ ref('int_jaffle_shop_orders_joined_to__payments') }}
